e54dd2a7556166ea4dfe7d285f7ffe24c85acb92
Sachin Kumar <sachin.kumar06@nagarro.com>
2020-06-30 16:41:50 +0530

    Initail skelton of project

---
+++ IdentityServer_Auth_API/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfDotNet.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FitstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
